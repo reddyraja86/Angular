8780a0213f2be8370300b9f7dc13687518b27236
Raju Rayappa <rrayappa@opentext.com>
2022-06-27 23:01:53 +0530

    Implemented Left panel

--- messenger-app/src/app/app.module.ts
+++ messenger-app/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DashComponent } from './dash/dash.component';
 import { LoginComponent } from './login/login.component';
+import { LeftpanelComponent } from './leftpanel/leftpanel.component';
+import { HeaderComponent } from './leftpanel/header/header.component';
+import { NameSearchComponent } from './leftpanel/name-search/name-search.component';
+import { ContactListComponent } from './leftpanel/contact-list/contact-list.component';
+import { ContactItemComponent } from './leftpanel/contact-list/contact-item/contact-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashComponent,
-    LoginComponent
+    LoginComponent,
+    LeftpanelComponent,
+    HeaderComponent,
+    NameSearchComponent,
+    ContactListComponent,
+    ContactItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ messenger-app/src/app/leftpanel/name-search/name-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-search',
+  templateUrl: './name-search.component.html',
+  styleUrls: ['./name-search.component.css']
+})
+export class NameSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
